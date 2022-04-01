4667d7eda1a55e23a8efe9c65908d3064770c39f
Alex Perez <acperez@syr.edu>
2022-04-01 13:53:19 -0400

    added some comments

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import cv2 as cv
 # connected pwm1(pin13) to pwm pin on bottom servo
 # https://avinton.com/en/academy/install-python2-7-opencv/
 
+
+# declare all the buttons as gpio
 displayButton = mraa.Gpio(16)
 terminateButton = mraa.Gpio(18)
 upButton = mraa.Gpio(21)
@@ -14,14 +16,15 @@ downButton = mraa.Gpio(22)
 leftButton = mraa.Gpio(38)
 rightButton = mraa.Gpio(40)
 
+# declare the motor pwm pins and set the period
 tiltMotor = mraa.Pwm(11)
 panMotor = mraa.Pwm(13)
 tiltMotor.period_ms(20)
 panMotor.period_ms(20)
-
 tiltMotor.enable(True)
 panMotor.enable(True)
 
+# set the direction of the buttons
 displayButton.dir(mraa.DIR_IN)
 terminateButton.dir(mraa.DIR_IN)
 upButton.dir(mraa.DIR_IN)
@@ -31,6 +34,7 @@ rightButton.dir(mraa.DIR_IN)
 
 
 def angletoPWM(angle):
+    # method to convert angle angle to pwm that is readable by the motors
     temp = (0.000556 * angle) + 0.88
     if temp == 0.90088:
         temp = 0.9800
@@ -41,6 +45,7 @@ def angletoPWM(angle):
 
 
 def testBothMotors():
+    # test: have both motors move from min to max value
     for i in range(0, 177):
         print("value of i passed:", i)
         print("converted", angletoPWM(i))
@@ -50,6 +55,7 @@ def testBothMotors():
 
 
 def tiltGoToPosition(val):
+    # used for bringing the tilt motor from 0 to a position val
     for i in range(0, val):
         print("value of i passed:", i)
         print("converted", angletoPWM(i))
@@ -59,6 +65,7 @@ def tiltGoToPosition(val):
 
 
 def panGoToPosition(val):
+    # used for bringing the pan motor from 0 to a position val
     for i in range(0, val):
         print("value of i passed:", i)
         print("converted", angletoPWM(i))
@@ -68,10 +75,12 @@ def panGoToPosition(val):
 
 
 def getValueOfPin(button):
+    # read the current value of a pin
     return button.read()
 
 
 def testTakeAPic():
+    # example code used for a signature. Take a screenshot and save it to a file when space is pressed.
     cam = cv.VideoCapture(4)
 
     cv.namedWindow("test")
@@ -102,55 +111,65 @@ def testTakeAPic():
 
 
 def main():
+    # main entry point for the program here
+
+    # global variables to track current position of tilt and pan motors
     currentPWMTILT = 0
     currentPWMPAN = 0
+
+    # change the motor step here (makes slower or faster depending on accuracy)
     motorStep = 5
 
+    # before doing anything, initalize both motors to positions 0
     panMotor.write(angletoPWM(0))
     tiltMotor.write(angletoPWM(0))
 
     while True:
+        # check what button is being pressed
         if getValueOfPin(displayButton):
             print('display button pressed!')
             panMotor.write(angletoPWM(45))
             time.sleep(0.2)
+
         elif getValueOfPin(terminateButton):
             print('terminate connection!')
             time.sleep(0.2)
 
         elif getValueOfPin(upButton):
             # print('move it up!')
-            if currentPWMTILT > 177-motorStep:
+            if currentPWMTILT > 177 - motorStep:
                 print('OUT OF BOUNDS! GOING UP CURRENT POSITION', currentPWMTILT)
                 pass
             else:
                 currentPWMTILT += motorStep
-
             tiltMotor.write(angletoPWM(currentPWMTILT))
             time.sleep(0.2)
+
         elif getValueOfPin(downButton):
             # print('move it down!')
-            print('OUT OF BOUNDS! GOING DOWN CURRENT POSITION', currentPWMTILT)
-            if currentPWMTILT < 0+motorStep:
+            if currentPWMTILT < 0 + motorStep:
+                print('OUT OF BOUNDS! GOING DOWN CURRENT POSITION', currentPWMTILT)
                 pass
             else:
                 currentPWMTILT -= motorStep
             # print('current pwm value tilt:', currentPWMTILT)
             tiltMotor.write(angletoPWM(currentPWMTILT))
             time.sleep(0.2)
+
         elif getValueOfPin(leftButton):
             # print('move it left!')
-            print('OUT OF BOUNDS! GOING LEFT CURRENT POSITION', currentPWMPAN)
-            if currentPWMPAN < 0+motorStep:
+            if currentPWMPAN < 0 + motorStep:
+                print('OUT OF BOUNDS! GOING LEFT CURRENT POSITION', currentPWMPAN)
                 pass
             else:
                 currentPWMPAN -= motorStep
             # print('current pwm value tilt:', currentPWMTILT)
             panMotor.write(angletoPWM(currentPWMPAN))
             time.sleep(0.2)
+
         elif getValueOfPin(rightButton):
             # print('move it right!')
-            if currentPWMPAN > 177-motorStep:
+            if currentPWMPAN > 177 - motorStep:
                 print('OUT OF BOUNDS! GOING RIGHT CURRENT POSITION', currentPWMPAN)
                 pass
             else:
@@ -158,12 +177,12 @@ def main():
             # print('current pwm value tilt:', currentPWMTILT)
             panMotor.write(angletoPWM(currentPWMPAN))
             time.sleep(0.2)
+
         else:
             # print('nothing pressed')
             time.sleep(0.2)
-
+            # testBothMotors()
             # takeAPic()
-
 
 
 if __name__ == '__main__':
