9a9a0776758c23a37a42360eae3b60cf11f1de76
ckaniero <71848723+ckaniero@users.noreply.github.com>
2022-04-04 15:44:12 -0400

    Code added to display the image captured by the camera on the client computer

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
+#import shutil
 import shutil
 import string
 import base64
 import zmq
-import cv2 as cv
+from PIL import Image
 import os
 
 context = zmq.Context()
@@ -10,7 +11,7 @@ context = zmq.Context()
 #Socket to talk to server
 print("Connecting to hello world server...")
 socket = context.socket(zmq.REQ)
-socket.connect("tcp://10.144.113.93:6969")
+socket.connect("tcp://10.144.113.4:6969")
 
 
 # testing
@@ -31,53 +32,70 @@ while 1:
         try:
             socket.send(b"getimage")
             message = socket.recv()
-            # f = open("receivedimage.jpg", 'wb')
-            # ba = bytearray(base64.b64decode(message))
-            # f.write(ba)
-            # f.close()
-            #
-            # image_dir = os.path.join('path', 'to', 'image')
-            # image_filename = 'image.jpg'
-            # full_image_path = os.path.join(image_dir, image_filename)
-            #
-            # image = cv.imread(full_image_path)
-            # if image is None:
-            #     shutil.copy(full_image_path, image_filename)
-            #     image = cv.imread(image_filename)
-            #     cv.imshow("Received Image", image)
-            #     os.remove(image_filename)
+            print(message.decode("utf-8"))
+            f = open("receivedimage.jpg", 'wb')
+            ba = bytearray(base64.b64decode(message))
+            f.write(ba)
+            f.close()
+
+
+            im = Image.open(r"receivedimage.jpg")
+            im.show()
+            print("Got here")
+            if im is None:
+                 #image = cv.imread(image_filename)
+                 #cv.imshow("Received Image", image)
+                 os.remove(im)
+
         except Exception as b:
             print('Error while sending the message!')
     elif userInput.lower() == 'getshapes':
         try:
             socket.send(b"getshapes")
             message = socket.recv()
+            print(message.decode("utf-8"))
         except Exception as b:
             print('Error while sending the message!')
     elif userInput.lower() == 'trackshape':
         try:
             socket.send(b"trackshape")
+            message = socket.recv()
+            print(message.decode("utf-8"))
         except Exception as b:
             print('Error while sending the message!')
     elif userInput.lower() == 'getangles':
         try:
             socket.send(b"getangles")
+            message = socket.recv()
+            print(message.decode("utf-8"))
         except Exception as b:
             print('Error while sending the message!')
     elif userInput.lower() == 'movepanangle':
         try:
             socket.send(b"movepanangle")
+            message = socket.recv()
+            print(message.decode("utf-8"))
         except Exception as b:
             print('Error while sending the message!')
     elif userInput.lower() == 'movetiltangle':
         try:
             socket.send(b"movetiltangle")
+            message = socket.recv()
+            print(message.decode("utf-8"))
         except Exception as b:
             print('Error while sending the message!')
     elif userInput.lower() == 'localcontrol':
         try:
             socket.send(b"localcontrol")
+            message = socket.recv()
+            print(message.decode("utf-8"))
         except Exception as b:
             print('Error while sending the message!')
     else:
         print('BAD USER INPUT! DID NOT SEND ANYTHING')
+
+def checkStatus(message):
+    if((message == 'trackshape done!') or (message == 'getimage done!') or (message == 'getangles done!') or (message == 'movetiltangle done!') or (message == 'movepanangle done!') or (message == 'localcontrol done!')):
+        print("Success!!")
+    else:
+        print("Unsuccessful")
